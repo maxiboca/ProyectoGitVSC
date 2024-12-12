118e53a72f3db9d11bcb32664036a6c6610e4c46
maxiboca <tevesmaxi971@gmail.com>
2024-12-12 23:50:24 +0100

    Creamos Saludo.java desde GitHub Maximiliano

---
+++ Saludo.java
@@ -0,0 +1,13 @@
+public class Saludo{
+
+ public static void saludo() {
+
+        System.out.println("Buenos dias Maximiliano Teves :D");
+        
+    }
+
+    public static void despedir() {
+        
+        System.out.println("Adiós, que tengas un buen día ");
+    }
+}
