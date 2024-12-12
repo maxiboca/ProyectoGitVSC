5c063bdd8f12eb66acbd1377b127a48be263abeb
maxiboca <tevesmaxi971@gmail.com>
2024-12-12 23:02:32 +0100

    Creamos Tarea.java, Maximiliano Teves

---
+++ Tareas.java
@@ -0,0 +1,13 @@
+public class Tareas {
+
+    public static void tarea() {
+
+        System.out.println("Tareas de Maximiliano Teves :D");
+        
+    }
+
+    public static void agregar() {
+        
+        System.out.println("Agregar tareas");
+    }
+}
